1d0ac31ac72b3e3ac7b4e93ab63121e72918cd3c
Tshepo <rocboyt@gmail.com>
2021-02-02 00:53:51 +0200

    Added round lose fragment

--- app/src/main/java/com/hearxgroup/tilttowin/features/game/fragments/RoundFinishedFragment.kt
+++ app/src/main/java/com/hearxgroup/tilttowin/features/game/fragments/RoundFinishedFragment.kt
@@ -1,60 +1,66 @@
 package com.hearxgroup.tilttowin.features.game.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.hearxgroup.tilttowin.R
+import com.hearxgroup.tilttowin.base.fragments.BaseDialogFragment
+import com.hearxgroup.tilttowin.databinding.FragmentRoundFinishedBinding
+import com.hearxgroup.tilttowin.features.game.GameActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RoundFinishedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RoundFinishedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class RoundFinishedFragment : BaseDialogFragment() {
+    lateinit var binding: FragmentRoundFinishedBinding
+    private lateinit var gameActivity: GameActivity
+    private lateinit var iconImg: ImageView
+    private lateinit var messageTv: TextView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_round_finished, container, false)
+    ): View {
+        isCancelable = false
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_round_finished,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+        binding.gameViewModel = gameActivity?.gameViewModel
+        val parentView = binding.root
+
+        initViews(parentView)
+        gameActivity.gameViewModel.roundCountDown {
+            dismiss()
+        }
+        return parentView
+    }
+
+    private fun initViews(parentView: View) {
+        iconImg = parentView.findViewById(R.id.imgIcon)
+        messageTv = parentView.findViewById(R.id.tvMessage)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        gameActivity = context as GameActivity
+    }
+
+    fun onBackPressed(): Boolean {
+        return  false
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RoundFinishedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RoundFinishedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(): RoundFinishedFragment {
+            val roundFinishedFragment = RoundFinishedFragment()
+            roundFinishedFragment.arguments = Bundle()
+            return  roundFinishedFragment
+        }
     }
 }
